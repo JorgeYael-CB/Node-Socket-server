e1131798089d8d42e2ecedcb9d86ec5525baf261
JorgeYael-CB <jorge.yael333@gmail.com>
2024-05-31 13:21:47 -0600

    new-ticket agregado

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,41 @@
+const newTicket = document.querySelector('.btn-secondary');
+const newTicketText = document.querySelector('#lbl-new-ticket');
+
+const apiUrl = 'http://localhost:3000/api';
+let isLoading = false;
 
 
-console.log('Nuevo Ticket HTML');
+//* Eventos
+newTicket.addEventListener('click', onCreateTicket);
+
+
+const showLastTicket = async() => {
+    if( !isLoading ){
+        const res = await fetch(`${apiUrl}/tickets/last`);
+        const lastTicket = await res.json();
+
+        newTicketText.textContent = `${lastTicket}`;
+    } else {
+        newTicketText.textContent = 'Loading...';
+    }
+};
+
+
+async function onCreateTicket() {
+    if( isLoading ) return console.log('Wait...');
+
+    isLoading = true;
+    showLastTicket();
+
+    await fetch(`${apiUrl}/tickets`, {
+        method: 'POST'
+
+    });
+
+    isLoading = false;
+    showLastTicket();
+};
+
+
+
+showLastTicket();
